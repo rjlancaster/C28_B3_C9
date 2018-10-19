a15eefc93afc9dfe4c3331767ea745ff1060fc73
rjlancaster <rjlancaster@gmail.com>
2018-10-18 21:05:59 -0500

    Took a shot at sending the food API to the DOM.  No luck yet

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+// 1. this function isn't working in the loop below.  Why??
+const foodAsHTML = (name, ethnicity, type) => `
+<div class="foods">
+  <h2>${name}</h2>
+  <p>${ethnicity}</p>
+  <p>${type}</p>
+</div>
+`
+
+fetch("http://localhost:8088/food/")
+.then(foods => foods.json())
+.then(json => {
+  console.table(json)
+  // 2 create const A which acts as fragment container.
+  const fragment = document.createDocumentFragment()
+  // 3  create const B that acts as query selector and grabs the ID or class.  you will be appending the child HTML content in some form or fashion to this const (probably usng another variable)
+  const addFoodToDom = document.querySelector(".foodList");
+  // 4  create loop to loop through D array
+  for (let i = 0; i < json.length; i++) {
+    // 5  create let C that will be used to create section element
+      let foodObject = json[i];
+      // 6 se C to attach to attach elements and what not.  Have it equal D looped which contains the meat . 
+      // debugger
+      let food = foodAsHTML(foodObject.name, foodObject.ethnicity, foodObject.type)
+      // 7 Set A.appendChild(C)
+      fragment.appendChild(food);
+      // 8 close loop
+    }
+    // 9 et B.appendChild(A)
+    addFoodToDom.appendChild(fragment);
+  })
+  
+  
+
